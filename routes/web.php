f0840d89a5fbda5060c1bfa494abfb18e79cc35d
huanghantao <2812240764@qq.com>
2017-07-13 21:06:03 -0700

    修改了路由

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,40 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', '\App\Http\Controllers\LoginController@index');
+
+//用户模块
+//注册页面
+Route::get('/register', '\App\Http\Controllers\RegisterController@index');
+//注册行为
+Route::post('/register', '\App\Http\Controllers\RegisterController@register');
+//登陆页面
+Route::get('/login', '\App\Http\Controllers\LoginController@index');
+//登陆行为
+Route::post('/login', '\App\Http\Controllers\LoginController@login');
+//登出行为
+Route::get('/logout', '\App\Http\Controllers\LoginController@logout');
+//个人设置页面
+Route::get('/user/me/setting', '\App\Http\Controllers\UserController@setting');
+//个人设置操作
+Route::post('/user/me/setting', '\App\Http\Controllers\UserController@settingStore');
+
+
+
+//文章模块
 Route::get('/posts', '\App\Http\Controllers\PostController@index');
 
 Route::get('/posts/create', '\App\Http\Controllers\PostController@create');
 
-Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');
-
 Route::post('/posts', '\App\Http\Controllers\PostController@store');
+
+Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');
 
 Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostController@edit');
 
 Route::put('/posts/{post}', '\App\Http\Controllers\PostController@update');
 
-Route::get('/posts/delete', '\App\Http\Controllers\PostController@delete');
+Route::get('/posts/{post}/delete', '\App\Http\Controllers\PostController@delete');
+
+Route::post('/posts/image/upload', '\App\Http\Controllers\PostController@imageUpload');
