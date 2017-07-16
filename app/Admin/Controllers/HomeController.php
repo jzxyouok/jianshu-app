8df7d5592cc683ec2fbee4770dfad34d26334b5d
huanghantao <2812240764@qq.com>
2017-07-16 07:19:04 -0700

    内容管理系统的控制器等等

---
+++ app/Admin/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+/**
+* 
+*/
+class HomeController extends Controller
+{
+	
+	public function index()
+	{
+		return view('admin/home/index');
+	}
+}
---
+++ app/Admin/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+/**
+* 
+*/
+class LoginController extends Controller
+{
+	public function index()
+	{
+		return view('admin/login/index');
+	}
+
+	public function login()
+	{
+	}
+
+	public function logout()
+	{
+
+	}
+}
