3de7234aa36a187315e9df06c3a57ed761c2090d
huanghantao <2812240764@qq.com>
2017-07-16 01:02:48 -0700

    完成posts表和topics表的关联

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 //it is posts table
 class Post extends Model
@@ -30,4 +31,24 @@ class Post extends Model
     {
         return $this->hasMany('App\Zan');
     }
+
+    //属于某个作者的文章
+    public function scopeAuthorBy(Builder $query, $user_id)
+    {
+        return $query->where('user_id', $user_id);
+    }
+
+    public function postTopics()
+    {
+        return $this->hasMany(\App\PostTopic::class, 'post_id', 'id');
+    }
+
+    //还不在这个专题的文章
+    public function scopeTopicNotBy(Builder $query, $topic_id)
+    {
+        return $query->doesntHave('postTopics', 'and', function($q) use($topic_id) {
+            $q->where('topic_id', $topic_id);
+        });
+    }
+
 }
